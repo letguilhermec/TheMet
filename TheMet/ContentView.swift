5e7f1612c2ec6e5f05924a0c48c4a358aebe2d60
Guilherme Correa <correacgui@gmail.com>
2023-05-08 11:30:29 -0300

    chore: testing SafariView

--- TheMet/ContentView.swift
+++ TheMet/ContentView.swift
@@ -5,10 +5,15 @@ struct ContentView: View {
   var body: some View {
     NavigationStack {
       List(store.objects, id: \.objectID) { object in
-//        NavigationLink(object.title) {
-//          ObjectView(object: object)
-//        }
-        Link(object.title, destination: URL(string: object.objectURL)!)
+        NavigationLink(
+          destination: SafariView(url: URL(string: object.objectURL)!)) {
+            HStack {
+              Text(object.title)
+              Spacer()
+              Image(systemName: "rectangle.portrait.and.arrow.right.fill")
+                .font(.footnote)
+            }
+          }
       }
       .navigationTitle("The Met")
     }
