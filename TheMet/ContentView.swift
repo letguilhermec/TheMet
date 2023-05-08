0fc4f4f911d04c965cff904eedf2d2438442841f
Guilherme Correa <correacgui@gmail.com>
2023-05-08 11:42:46 -0300

    feat: renders navlinks according to isPublicDomain

--- TheMet/ContentView.swift
+++ TheMet/ContentView.swift
@@ -5,10 +5,16 @@ struct ContentView: View {
   var body: some View {
     NavigationStack {
       List(store.objects, id: \.objectID) { object in
-        NavigationLink(
-          destination: SafariView(url: URL(string: object.objectURL)!)) {
+        if !object.isPublicDomain,
+           let url = URL(string: object.objectURL) {
+          NavigationLink(destination: SafariView(url: url)) {
             WebIndicatorView(title: object.title)
           }
+        } else {
+          NavigationLink(object.title) {
+            ObjectView(object: object)
+          }
+        }
       }
       .navigationTitle("The Met")
     }
