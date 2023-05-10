cb265d5be4d0e0b4de8773d5f7b7e873e3773eee
Guilherme Correa <correacgui@gmail.com>
2023-05-10 13:51:45 -0300

    feat: target Object to widget and mock data

--- TheMetWidget/TheMetWidget.swift
+++ TheMetWidget/TheMetWidget.swift
@@ -1,94 +1,63 @@
-/// Copyright (c) 2023 Kodeco LLC
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-/// 
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-/// 
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import WidgetKit
 import SwiftUI
 
 struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+  func placeholder(in context: Context) -> SimpleEntry {
+    SimpleEntry(date: Date())
+  }
+  
+  func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+    let entry = SimpleEntry(date: Date())
+    completion(entry)
+  }
+  
+  func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+    var entries: [SimpleEntry] = []
+    
+    // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+    let currentDate = Date()
+    for hourOffset in 0 ..< 5 {
+      let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+      let entry = SimpleEntry(date: entryDate)
+      entries.append(entry)
     }
-
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
-        completion(entry)
-    }
-
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-    }
+    
+    let timeline = Timeline(entries: entries, policy: .atEnd)
+    completion(timeline)
+  }
 }
 
 struct SimpleEntry: TimelineEntry {
-    let date: Date
+  let date: Date
+  let object: Object
 }
 
 struct TheMetWidgetEntryView : View {
-    var entry: Provider.Entry
-
-    var body: some View {
-        Text(entry.date, style: .time)
-    }
+  var entry: Provider.Entry
+  
+  var body: some View {
+    Text(entry.date, style: .time)
+  }
 }
 
 struct TheMetWidget: Widget {
-    let kind: String = "TheMetWidget"
-
-    var body: some WidgetConfiguration {
-        StaticConfiguration(
-          kind: kind,
-          provider: Provider()
-        ) { entry in
-            TheMetWidgetEntryView(entry: entry)
-        }
-        .configurationDisplayName("The Met")
-        .description("View Objects from the Metropolitan Museum.")
+  let kind: String = "TheMetWidget"
+  
+  var body: some WidgetConfiguration {
+    StaticConfiguration(
+      kind: kind,
+      provider: Provider()
+    ) { entry in
+      TheMetWidgetEntryView(entry: entry)
     }
+    .configurationDisplayName("The Met")
+    .description("View Objects from the Metropolitan Museum.")
+  }
 }
 
 struct TheMetWidget_Previews: PreviewProvider {
-    static var previews: some View {
-        TheMetWidgetEntryView(entry: SimpleEntry(date: Date()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
-    }
+  static var previews: some View {
+    TheMetWidgetEntryView(entry: SimpleEntry(date: Date()))
+      .previewContext(WidgetPreviewContext(family: .systemSmall))
+  }
 }
