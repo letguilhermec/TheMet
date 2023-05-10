56d1fdc4ab7af5ce97de05a9b125c037a44ce3c3
Guilherme Correa <correacgui@gmail.com>
2023-05-10 14:42:00 -0300

    feat: working widget

--- TheMetWidget/TheMetWidget.swift
+++ TheMetWidget/TheMetWidget.swift
@@ -3,22 +3,22 @@ import SwiftUI
 
 struct Provider: TimelineProvider {
   func placeholder(in context: Context) -> SimpleEntry {
-    SimpleEntry(date: Date())
+    SimpleEntry(date: Date(), object: Object.sample(isPublicDomain: true))
   }
   
   func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-    let entry = SimpleEntry(date: Date())
+    let entry = SimpleEntry(date: Date(), object: Object.sample(isPublicDomain: false))
     completion(entry)
   }
   
-  func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+  func getTimeline(in context: Context, completion: @escaping (Timeline<SimpleEntry>) -> ()) {
     var entries: [SimpleEntry] = []
     
     // Generate a timeline consisting of five entries an hour apart, starting from the current date.
     let currentDate = Date()
     for hourOffset in 0 ..< 5 {
       let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-      let entry = SimpleEntry(date: entryDate)
+      let entry = SimpleEntry(date: entryDate, object: Object.sample(isPublicDomain: true))
       entries.append(entry)
     }
     
@@ -36,7 +36,7 @@ struct TheMetWidgetEntryView : View {
   var entry: Provider.Entry
   
   var body: some View {
-    Text(entry.date, style: .time)
+    WidgetView(entry: entry)
   }
 }
 
@@ -52,12 +52,13 @@ struct TheMetWidget: Widget {
     }
     .configurationDisplayName("The Met")
     .description("View Objects from the Metropolitan Museum.")
+    .supportedFamilies([.systemMedium, .systemLarge])
   }
 }
 
 struct TheMetWidget_Previews: PreviewProvider {
   static var previews: some View {
-    TheMetWidgetEntryView(entry: SimpleEntry(date: Date()))
-      .previewContext(WidgetPreviewContext(family: .systemSmall))
+    TheMetWidgetEntryView(entry: SimpleEntry(date: Date(), object: Object.sample(isPublicDomain: true)))
+      .previewContext(WidgetPreviewContext(family: .systemMedium))
   }
 }
